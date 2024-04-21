34016d1bb6e370aa4c2151246b422f2b8f80e62f
Brian Kwong <bkwong01@calpoly.edu>
2024-04-21 01:59:41 -0700

    updates

--- popup.js
+++ popup.js
@@ -1,69 +1,68 @@
-const onButton = document.getElementById("on");
-const offButton = document.getElementById("off");
+const onButton = document.getElementById("switch");
 
+let state = false; // Initialize state variable
 import { extractDomain, makeNewTabGroup } from "./background.js";
+onButton.addEventListener("change", function () {
+  state = !state; // Toggle the state when the button is changed
+  if (state === true) {
+    chrome.storage.local.set({ enabled: enabled }).then(() => {
+      var myTabs = [];
 
-onButton.onclick = function () {
-  let enabled = true;
-  chrome.storage.local.set({ enabled: enabled }).then(() => {
-    var myTabs = [];
+      // const classifyNew = async (newTab) => {
+      //   const url = "http://127.0.0.1:5000/?query=" + newTab;
+      // };
+      // classifyNew("test");
+      chrome.storage.local.get(["enabled"], (enabled) => {
+        var enabled = enabled["enabled"];
 
-    // const classifyNew = async (newTab) => {
-    //   const url = "http://127.0.0.1:5000/?query=" + newTab;
-    // };
-    // classifyNew("test");
-    chrome.storage.local.get(["enabled"], (enabled) => {
-      var enabled = enabled["enabled"];
-
-      function filterGroups(listOfTitle) {
-        var myGroups = [];
-        while (listOfTitle.length > 0) {
-          var group = [];
-          var base = listOfTitle.pop();
-          group.push(base);
-          var base_url, given_url;
-          for (var i = listOfTitle.length - 1; i >= 0; i--) {
-            base_url = extractDomain(base["url"]);
-            given_url = extractDomain(listOfTitle[i]["url"]);
-            if (base_url === given_url) {
-              group.push(listOfTitle[i]);
-              listOfTitle.splice(i, 1);
+        function filterGroups(listOfTitle) {
+          var myGroups = [];
+          while (listOfTitle.length > 0) {
+            var group = [];
+            var base = listOfTitle.pop();
+            group.push(base);
+            var base_url, given_url;
+            for (var i = listOfTitle.length - 1; i >= 0; i--) {
+              base_url = extractDomain(base["url"]);
+              given_url = extractDomain(listOfTitle[i]["url"]);
+              if (base_url === given_url) {
+                group.push(listOfTitle[i]);
+                listOfTitle.splice(i, 1);
+              }
             }
+            myGroups.push(group);
           }
-          myGroups.push(group);
+          return myGroups;
         }
-        return myGroups;
-      }
 
-      if (enabled) {
-        var tabs = chrome.tabs.query({}, (tabs) => {
-          tabs.forEach((tab) => {
-            myTabs.push(tab);
+        if (enabled) {
+          var tabs = chrome.tabs.query({}, (tabs) => {
+            tabs.forEach((tab) => {
+              myTabs.push(tab);
+            });
+            console.log(myTabs);
+            var groupsTabs = filterGroups(myTabs.map((tab) => tab));
+            groupsTabs.forEach((group) => {
+              makeNewTabGroup(group);
+            });
           });
-          console.log(myTabs);
-          var groupsTabs = filterGroups(myTabs.map((tab) => tab));
-          groupsTabs.forEach((group) => {
-            makeNewTabGroup(group);
-          });
-        });
-      }
+        }
+      });
     });
-  });
-};
-
-offButton.onclick = function () {
-  let enabled = false;
-  chrome.storage.local.set({ enabled: enabled }).then(() => {
-    chrome.tabGroups.query({}, (groups) => {
-      if (groups.length === 0) return;
-      groups.forEach((group) => {
-        chrome.tabs.query({ groupId: group["id"] }, (tabs) => {
-          chrome.tabs.ungroup(
-            tabs.map((tab) => tab["id"]),
-            () => {}
-          );
+  } else {
+    let enabled = false;
+    chrome.storage.local.set({ enabled: enabled }).then(() => {
+      chrome.tabGroups.query({}, (groups) => {
+        if (groups.length === 0) return;
+        groups.forEach((group) => {
+          chrome.tabs.query({ groupId: group["id"] }, (tabs) => {
+            chrome.tabs.ungroup(
+              tabs.map((tab) => tab["id"]),
+              () => {}
+            );
+          });
         });
       });
     });
-  });
-};
+  }
+});
