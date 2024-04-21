5f130eec5ed8cee6e123b841017ab17c49da5d2c
Brian Kwong <bkwong01@calpoly.edu>
2024-04-20 18:28:44 -0700

    Added chrome tabs

--- background.js
+++ background.js
@@ -1,12 +1,40 @@
-try {
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete") {
-      chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["content.js"],
-      });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  var myTabs = [];
+
+  function filterGroups(listOfTitle) {
+    var myGroups = [];
+    while (listOfTitle.length > 0) {
+      var group = [];
+      var base = listOfTitle.pop();
+      group.push(base);
+      for (var i = 0; i < listOfTitle.length; i++) {
+        if (listOfTitle[i]["title"] === base["title"]) {
+          group.push(listOfTitle[i]);
+          listOfTitle.splice(i, 1);
+        }
+      }
+      myGroups.push(group);
     }
-  });
-} catch (e) {
-  console.log(e);
-}
+    return myGroups;
+  }
+
+  if (changeInfo.status === "complete") {
+    var tabs = chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        myTabs.push(tab);
+      });
+      console.log(myTabs);
+      groupsTabs = filterGroups(myTabs.map((tab) => tab));
+      groupsTabs.forEach((group) => {
+        chrome.tabs.group(
+          {
+            tabIds: group.map((tab) => tab.id),
+          },
+          (groupId) => {
+            chrome.tabGroups.update(groupId, { title: group[0].title });
+          }
+        );
+      });
+    });
+  }
+});
--- content.js
+++ content.js
@@ -1,12 +1,7 @@
-// Access the DOM.
-var x = document.getElementsByTagName("img");
-for (var i = 0; i < x.length; i++) {
-  alert(x[i].className);
-  x[i].addEventListener(
-    "click",
-    () => {
-      alert("Hello World!");
-    },
-    false
-  );
-}
+var windows = chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true,
+});
+windows.then((tabs) => {
+  console.log(tabs);
+});
